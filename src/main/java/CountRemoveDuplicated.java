c6a479d39b99cb80ba03a192c917a6aef9ac7c97
darrylsun <darrylsun@tencent.com>
2023-08-10 16:28:20 +0800

    fix test

--- src/main/java/CountRemoveDuplicated.java
+++ src/main/java/CountRemoveDuplicated.java
@@ -1,28 +1,29 @@
 /**
  * @Author: darrylsun
  * @Description: 给一个有序的数组，统计这个数组中去重后的个数；保证空间复杂度 O(1)
- * 如：数组：[1,3,3,6,8,9,9]
- * 输出：5
+ *         如：数组：[1,3,3,6,8,9,9]
+ *         输出：5
  * @Date: 2023/8/9
  */
 public class CountRemoveDuplicated {
 
     public static void main(String[] args) {
-        int[] intArr = {1,3,3,4,6,7,8,8,9,9,9};
+        int[] intArr = {1, 3, 3, 4, 6, 7, 8, 8, 9, 9, 9};
         System.out.println(countRemoveDuplicated(intArr));
     }
 
     /**
      * 统计数组去重后总个数
+     *
      * @param intArr
      * @return
      */
     public static int countRemoveDuplicated(int[] intArr) {
         int i = 0, j = 1;
-        for ( ; j < intArr.length; ) {
+        for (; j < intArr.length; ) {
             if (intArr[i] != intArr[j]) {
                 // 用于下一轮的比较
-                intArr[i+1] = intArr[j];
+                intArr[i + 1] = intArr[j];
                 i++;
                 j++;
             } else {
